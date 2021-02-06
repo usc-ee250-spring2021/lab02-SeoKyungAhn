fb7a40d576005ac6b38e0d6932df025983752b32
SeoKyungAhn <seokyuna@usc.edu>
2021-02-06 08:57:03 -0800

    fixed the string casting

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -42,5 +42,5 @@ if __name__ == '__main__':
 
         print(grovepi.ultrasonicRead(PORT))
 	ultrasonicVal = grovepi.ultrasonicRead(PORT)
-	str(ultrasonicVal)
-	setText_noRefresh(ultrasonicVal" cm")
+	
+	setText_noRefresh(str(ultrasonicVal)+" cm")
